aeb3d7948b3915342fd2c76716cf9587a0490284
Harkaitz Agirre Ezama <harkaitzv@gmail.com>
2024-02-05 01:59:46 +0100

    m

--- day.go
+++ day.go
@@ -4,6 +4,22 @@ import (
 	"time"
 )
 
+// Day parses a YYYY-MM-DD formatted text and panics on error.
+func Day(text string) (t time.Time) {
+	var err error
+	t, err = DayParse(text, "en")
+	if err != nil { panic(err) }
+	return
+}
+
+// DayFormat print a good date format for a specific language.
+func DayFormat(language string) (format string) {
+	switch language {
+	case "es": return "02/01/2006"
+	default:   return "2006-01-02"
+	}
+}
+
 // DayParse parses a YYYY-MM-DD formatted text.
 func DayParse(text, language string) (t time.Time, err error) {
 	t, err = time.Parse("2006-01-02", text)
@@ -12,5 +28,5 @@ func DayParse(text, language string) (t time.Time, err error) {
 
 // DayShow shows a date in YYYY-MM-DD format.
 func DayShow(t time.Time, language string) string {
-	return t.Format("2006-01-02")
+	return t.Format(DayFormat(language))
 }
